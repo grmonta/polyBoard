a3acca2436252d0c0e89f69df3456bc02cf86841
Grmonta <grmonta@gmail.com>
2019-08-18 18:17:08 -0500

    commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/supplies', require('./routes/api/supplies'));
 //serve satis assests in production
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.statis('client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
